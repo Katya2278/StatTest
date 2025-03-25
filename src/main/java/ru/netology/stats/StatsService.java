f6cf0e4089654357b8fdf430c7cc7f877e2e07e0
katya <katerink11@yandex.ru>
2025-03-25 08:00:39 +0300

    initial

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,66 @@
+package ru.netology.stats;
+
+import java.util.Arrays;
+import java.util.concurrent.RecursiveTask;
+
+public class StatsService {
+    public int peakSels(int[] sales) {
+        int peakday = 0;
+        for (int i = 0; i < sales.length; i++) {
+
+            if (sales[i] >= sales[(int) peakday]) {
+                peakday = i;
+            }
+        }
+        return peakday + 1;
+    }
+
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        long totalSale = sum(sales);
+        long averegeSale = totalSale / 12;
+        return averegeSale;
+    }
+    public int minSels(int[] sales) {
+        int minday = 0;
+        for (int i = 0; i < sales.length; i++) {
+
+            if (sales[i] <= sales[(int) minday]) {
+                minday = i;
+            }
+        }
+        return minday + 1;
+    }
+    public int below (long[]sales) {
+        long averageSale = average(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < averageSale) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
+    public int above (long[]sales) {
+        long averageSale = average(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > averageSale) {
+                counter++;
+            }
+        }
+        return counter;
+
+
+    }
+}
---
+++ src/test/java/StatservisTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.stats.StatsService;
+
+public class StatservisTest {
+    @Test
+    public void MonthofBigSales() {
+        StatsService service = new StatsService();
+        int[] summ = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.peakSels(summ);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSumm() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        long actual = service.sum(sales);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void averegetest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18 / 12;
+        long actual = service.average(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void MonthofminSales() {
+        StatsService service = new StatsService();
+        int[] summ = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.minSels(summ);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void belowaverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.below(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void aboveaveragemonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.above(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
