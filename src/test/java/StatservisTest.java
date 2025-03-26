5207e2e7ef333c3a322e893548f5f1d9565ded4e
katya <katerink11@yandex.ru>
2025-03-26 17:28:45 +0300

    correctcod

--- src/test/java/StatservisTest.java
+++ src/test/java/StatservisTest.java
@@ -17,7 +17,7 @@ public class StatservisTest {
     public void testSumm() {
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 8+15+ 13+ 15+ 17+ 20+ 19+ 20+ 7+ 14+ 14+18;
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
         long actual = service.sum(sales);
         Assertions.assertEquals(expected, actual);
 
